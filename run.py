50c0d603679beaf70f14a183d12bdb0a3eb556fe
qy53zuny <joschua.schilling@fau.de>
2021-05-10 14:14:38 +0200

    enable folder specification for run.py to allow multiprocessing

--- run.py
+++ run.py
@@ -8,9 +8,18 @@ def parseArgs(parser):
     parser.add_argument('file', action='store', help='file that should be analyzed')
     parser.add_argument('-c', action='store_true', dest='compile', default=False, help='use compilation in Soufflé')
     parser.add_argument('-p', action='store_true', dest='profile', default=False, help='use the Soufflé profiler')
+    parser.add_argument('-f', action='store', dest='facts_dir', default="facts",
+                        help='specify facts directory (default: facts)')
+    parser.add_argument('-o', action='store', dest='output_dir', default="output",
+                        help='specify output directory (default: output)')
+    parser.add_argument('-j', action='store', dest='thread_count', default="4",
+                        help='specify how many threads Soufflé may use (default: 4)')
 
 
-def parse(filepath):
+def parse(filepath, facts_dir):
+
+    if(not os.path.isdir(facts_dir)):
+        os.mkdir(facts_dir)
 
     folder, filename = os.path.split(filepath)
     filename = filename[:-2]
@@ -18,23 +27,31 @@ def parse(filepath):
     if(filepath.endswith(".c")):
         command = "clang -S -emit-llvm \""+filepath+"\" -o \"./llvm-ir/"+filename+".ll\""
         os.system(command)
-        command2 = "python3 Parser.py ./llvm-ir/"+filename+".ll"
+        command2 = "python3 Parser.py ./llvm-ir/"+filename+".ll "
+        if(facts_dir):
+            command2 += "-o "+facts_dir
         subprocess.call(command2, shell=True)
     elif(filepath.endswith(".ll") or filepath.endswith(".bc")):
-        command = "python3 Parser.py "+filepath
+        command = "python3 Parser.py "+filepath+" "
+        if(facts_dir):
+            command += "-o "+facts_dir
         subprocess.call(command, shell=True)
     else:
         print("Unsupported file type. File extension needs to be .c, .bc or .ll")
 
 
-def run(filepath, compile, profile):
-    command = "souffle \"./logic/main.dl\" "
+def run(filepath, compile, profile, facts_dir, output_dir, thread_count):
+
+    if(not os.path.isdir(output_dir)):
+        os.mkdir(output_dir)
+
+    command = "souffle \"logic/main.dl\" "
     if(compile):
         command += "--compile "
-    command += "-F \"./facts\" -D \"./output\" "
+    command += "-F \""+facts_dir+"\" -D \""+output_dir+"\" "
     if(profile):
         command += "-p ./profile "
-    command += "-j 4"
+    command += "-j "+thread_count
 
     subprocess.call(command, shell=True)
 
@@ -53,8 +70,14 @@ def main():
     compile = args.compile
     profile = args.profile
 
-    parse(filepath)
-    run(filepath, compile, profile)
+    facts_dir = args.facts_dir
+    output_dir = args.output_dir
+    thread_count = args.thread_count
+    if(not thread_count):
+        thread_count = 4
+
+    parse(filepath, facts_dir)
+    run(filepath, compile, profile, facts_dir, output_dir, thread_count)
 
 
 if __name__ == "__main__":
