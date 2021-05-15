02dc65786f28f7629a99b9512f522f70111a529a
qy53zuny <joschua.schilling@fau.de>
2021-05-15 19:33:56 +0200

    alias: case if %x<=>%y => %x_z <=> %y_z

--- logic/functors/functors.c
+++ logic/functors/functors.c
@@ -1,12 +1,89 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
 #include <cstdint>
+
 extern "C" {
 
 int32_t f(int32_t x) {
     return x + 1;
 }
 
-const char *g() {
-    return "Hello world";
+const char *get_before_underscore(char *input) {
+	char* input2 = (char*) malloc(strlen(input)*sizeof(char));
+	strcpy(input2, input);
+	
+	const char deli = '_';
+	char * const separate_at = strchr(input2, deli);
+
+	if(separate_at != NULL) {
+		*separate_at = '\0';
+	}
+
+	return input2;
+	
 }
+
+const char *get_behind_underscore(char *input) {
+	char* input2 = (char*) malloc(strlen(input)*sizeof(char));
+	strcpy(input2, input);
+	
+	const char deli = '_';
+	char * const separate_at = strchr(input2, deli);
+
+	if(separate_at != NULL) {
+		return separate_at+1;
+	}
+
+	return "";
+	
+}
+
+const char *remove_one_zero(char * strInput)
+{
+
+	char* str = (char*) malloc(strlen(strInput)*sizeof(char));
+	strcpy(str, strInput);
+
+    int i, j;
+    int len, removeLen;
+    int found = 0;
+
+    len = strlen(str);
+	const char * toRemove = "0_";
+
+    removeLen = strlen(toRemove);
+
+    for(i=0; i<len; i++)
+    {
+        found = 1;
+        for(j=0; j<removeLen; j++)
+        {
+            if(str[i+j] != toRemove[j])
+            {
+                found = 0;
+                break;
+            }
+        }
+
+        // If word has been found then remove it by shifting characters
+        if(found == 1)
+        {
+            for(j=i; j<=len-removeLen; j++)
+            {
+                str[j] = str[j + removeLen];
+            }
+
+            // Terminate from loop so only first occurrence is removed
+            break;
+        }
+    }
+
+	// printf("before:%s\n", strInput);
+	// printf("after:%s\n", str);
+
+	return str;
+}
+
 
 }
