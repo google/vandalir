6f53db268a3ff54e10d69c0842a6ad4740202d52
qy53zuny <joschua.schilling@fau.de>
2021-07-11 11:20:05 +0200

    rewrite of memlocation structure handling

--- logic/functors/functors.c
+++ logic/functors/functors.c
@@ -54,11 +54,61 @@ int32_t get_before_seperator(char *input) {
 	
 }
 
+int32_t get_before_xseperator(char *input) {
+
+	char *e;
+	e = strchr(input, 'x');
+	
+	if(e == NULL) {
+		return -1;
+	}
+
+	int32_t x;
+	x = (int32_t)(e - input);
+	return x;
+	
+}
+
+int32_t get_num_seperators(char *input) {
+
+	int32_t i;
+
+	for (i=0; input[i]; input[i]=='~' ? i++ : *input++);
+	
+	return i;
+	
+}
+
+int32_t get_num_xseperators(char *input) {
+
+	int32_t i;
+
+	for (i=0; input[i]; input[i]=='x' ? i++ : *input++);
+	
+	return i;
+	
+}
+
 const char *get_behind_seperator(char *input) {
 	char* input2 = (char*) malloc(strlen(input)*sizeof(char));
 	strcpy(input2, input);
 	
 	const char deli = '~';
+	char * const separate_at = strchr(input2, deli);
+
+	if(separate_at != NULL) {
+		return separate_at+1;
+	}
+
+	return "";
+	
+}
+
+const char *get_behind_xseperator(char *input) {
+	char* input2 = (char*) malloc(strlen(input)*sizeof(char));
+	strcpy(input2, input);
+	
+	const char deli = 'x';
 	char * const separate_at = strchr(input2, deli);
 
 	if(separate_at != NULL) {
