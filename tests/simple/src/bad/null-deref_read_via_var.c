08c5053973f158d31ff7a64e321811fb7c7a5d98
Dmitry Yatsushkevich <dmitryya@google.com>
2023-01-18 16:47:04 -0800

    logic: vulns: add basic null dereference detection rules
    Add basic rules to detect null dereference.

--- tests/simple/src/bad/null-deref_read_via_var.c
+++ tests/simple/src/bad/null-deref_read_via_var.c
@@ -23,22 +23,7 @@
 
 int main() {
   int *ptr = NULL;
-  int i = 1;
-  int b = 5;
 
-  if (ptr == NULL) {
-    return 1;
-  }
-
-  if (i != 0) {
-    b++;
-  }
-  if (b > 5) {
-    i = 0;
-  }
-
-  if (i == 0) {
-    *ptr = 0xdeadbeef;
-  }
+  int s = *ptr;
   return 0;
 }
